933ef831bb5395116d782c7714281578677a0fc1
Frank Förster <foersterfrank@gmx.de>
2015-12-03 08:24:58 +0100

    Opening files

--- kmer_intersection/generate_kmer_intersection.pl
+++ kmer_intersection/generate_kmer_intersection.pl
@@ -10,4 +10,15 @@ $files{500} = shift;
 $files{800} = shift;
 $files{moleculo} = shift;
 
+foreach my $file (keys %files)
+{
+    $files{$file}{name} = $files{$file};
+    open($files{$file}{fh}, "<", $files{$file}{name}) || die "Unable to open file '".$files{$file}{name}."': $!";
+}
+
+
+foreach my $file (keys %files)
+{
+    close($files{$file}{fh}) || die "Unable to close file '".$files{$file}{name}."': $!";
+}
 
