703954ae398645c628f0763520099e5073be8a21
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-12-03 11:51:33 +0100

    Implementing the functionality

--- kmer_intersection/split_kmer_dump.pl
+++ kmer_intersection/split_kmer_dump.pl
@@ -9,4 +9,19 @@ my %dataset = ();
 
 open(FH, "<", $file) || die "Unable to open file '$file': $!";
 
+while (<FH>)
+{
+    my $tag = substr($_, 0, 2);
+
+    $dataset{$tag} .= $_;
+}
+
 close(FH) || die "Unable to close file '$file': $!";
+
+foreach my $tag (keys %dataset)
+{
+    my $file_tagged = $file.$tag;
+    open(FH, ">", $file_tagged) || die "Unable to open file '$file_tagged': $!";
+    print FH $dataset{$tag};
+    close(FH) || die "Unable to close file '$file_tagged': $!";
+}
