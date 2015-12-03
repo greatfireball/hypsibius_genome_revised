0c49a0872bb25ca3bb84ba316767e8301ee7d46a
Frank Förster <foersterfrank@gmx.de>
2015-12-03 08:31:05 +0100

    Reading from files if necessary

--- kmer_intersection/generate_kmer_intersection.pl
+++ kmer_intersection/generate_kmer_intersection.pl
@@ -10,14 +10,32 @@ $files{500} = shift;
 $files{800} = shift;
 $files{moleculo} = shift;
 
-foreach my $file (keys %files)
+my @order = qw(300 500 800 moleculo);
+
+foreach my $file (@order)
 {
     $files{$file}{name} = $files{$file};
+    $files{$file}{line} = undef;
     open($files{$file}{fh}, "<", $files{$file}{name}) || die "Unable to open file '".$files{$file}{name}."': $!";
 }
 
+while (grep {! eof($files{$_}{fh})}  (@order))
+{
+    # which file I have to read?
+    foreach my $file (@order)
+    {
+	# read the next line from the file if not eof and the line value is undef
+	unless (defined $files{$file}{line} && ! eof($files{$file}{fh}))
+	{
+	    $files{$file}{line} = <$files{$file}{fh}>;
+	    chomp($files{$file}{line});
+	}
+    }
+    
+}
 
-foreach my $file (keys %files)
+
+foreach my $file (@order)
 {
     close($files{$file}{fh}) || die "Unable to close file '".$files{$file}{name}."': $!";
 }
