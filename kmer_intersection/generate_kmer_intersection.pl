c23216fb7f15db0d1ff824766f593bea47721cba
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-12-03 10:13:37 +0100

    Cleaned code using perltidy

--- kmer_intersection/generate_kmer_intersection.pl
+++ kmer_intersection/generate_kmer_intersection.pl
@@ -5,44 +5,41 @@ use warnings;
 
 my %files = ();
 
-$files{300} = shift;
-$files{500} = shift;
-$files{800} = shift;
+$files{300}      = shift;
+$files{500}      = shift;
+$files{800}      = shift;
 $files{moleculo} = shift;
 
 my @order = qw(300 500 800 moleculo);
 
-foreach my $file (@order)
-{
+foreach my $file (@order) {
     my $name = $files{$file};
-    $files{$file} = {name => $name};
+    $files{$file} = { name => $name };
     $files{$file}{line} = undef;
-    open(my $fh, "<", $files{$file}{name}) || die "Unable to open file '".$files{$file}{name}."': $!";
+    open( my $fh, "<", $files{$file}{name} )
+      || die "Unable to open file '" . $files{$file}{name} . "': $!";
     $files{$file}{fh} = $fh;
 }
 
-while (grep {! eof($files{$_}{fh})}  (@order))
-{
+while ( grep { !eof( $files{$_}{fh} ) } (@order) ) {
+
     # which file I have to read?
-    foreach my $file (@order)
-    {
-	# read the next line from the file if not eof and the line value is undef
-	unless (defined $files{$file}{line} && eof($files{$file}{fh}))
-	{
-	    my $fh = $files{$file}{fh};
-	    $files{$file}{line} = <$fh>;
-	    chomp($files{$file}{line});
-	    ($files{$file}{kmer},$files{$file}{count}) = split(/\t/, $files{$file}{line});
-	}
+    foreach my $file (@order) {
+
+       # read the next line from the file if not eof and the line value is undef
+        unless ( defined $files{$file}{line} && eof( $files{$file}{fh} ) ) {
+            my $fh = $files{$file}{fh};
+            $files{$file}{line} = <$fh>;
+            chomp( $files{$file}{line} );
+            ( $files{$file}{kmer}, $files{$file}{count} ) =
+              split( /\t/, $files{$file}{line} );
+        }
     }
 
-    
-    
 }
 
-
-foreach my $file (@order)
-{
-    close($files{$file}{fh}) || die "Unable to close file '".$files{$file}{name}."': $!";
+foreach my $file (@order) {
+    close( $files{$file}{fh} )
+      || die "Unable to close file '" . $files{$file}{name} . "': $!";
 }
 
