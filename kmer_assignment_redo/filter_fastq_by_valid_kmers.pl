6cf9c21ec4f296a5a9fbd4b7d3e7ddc21ac1d4b0
Frank Förster <frank.foerster@biozentrum.uni-wuerzburg.de>
2015-12-07 11:17:34 +0100

    New version of the kmer filter program

---
+++ kmer_assignment_redo/filter_fastq_by_valid_kmers.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+our $VERSION = '0.1';
+
+my %options = ();
+
+__END__
+
+=pod
+
+=head1 NAME
+
+filter_fastq_by_valid_mers.pl - Perl script to filter the content of a fastq file based on the occurence of its sequence kmers in different libraries
+
+=head1 VERSION
+
+This is the documentation for version 0.1
+
+=head1 SYNOPSIS
+
+    ./filter_fastq_by_valid_kmers.pl \
+       --in input.fastq \
+       --out filtered.fastq \
+       --kmerlib LIB1=lib1_mer_19 \
+       --kmerlib LIB2=lib2_mer_19_A,lib2_mer_19_B
+
+=head1 DESCRIPTION
+
+We want to filter reads from a fastq based on the occurence of the
+kmers of its sequences be represented by multiple libraries.
+
+=head1 PARAMETER
+
+=over 4
+
+=item --input FILE(S)
+
+This gives the input file(s) which contain the sequences to be filtered.
+This is mendatory, but can be multiple input files.
+
+=item --output FILE
+
+This file is used for the output of the filtering. If no file is given
+the output will be put on STDOUT
+
+=item --kmerlib FILE(S)
+
+Indicate the location of kmer-hash file(s) belonging to one single
+group of kmers. I assume that the kmer is present in all hashs of the
+library. If not, it will result in a warning message, but the kmer is
+still valid if present in all Libraries. Count values for kmers will
+be reported for whole libraries. Valid kmers have to be represented in
+all defined libraries.
+
+=back
+
+=head1 SEE ALSO
+
+TODO
+
+=head1 AUTHOR
+
+Frank Foerster (E<lt>frank.foerster@uni-wuerzburg.deE<gt>)
+
+=head1 LICENCE
+
+TODO
+
+=head1 HISTORY
+
+=cut
