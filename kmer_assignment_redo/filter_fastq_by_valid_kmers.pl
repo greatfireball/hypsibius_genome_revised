4f3650009d94a2601b9726e9cbef55923fea066a
Frank Förster <foersterfrank@gmx.de>
2015-12-22 04:43:11 +0100

    Filter fastq capable to work with paired end data

--- kmer_assignment_redo/filter_fastq_by_valid_kmers.pl
+++ kmer_assignment_redo/filter_fastq_by_valid_kmers.pl
@@ -17,11 +17,18 @@ my %missing_kmers = ();
 GetOptions(
     'i|infile=s@' => \$options{inputfiles},
     'o|output=s'  => \$options{outputfile},
-    'k|kmerhash=s' => \$options{kmerhash}
+    'k|kmerhash=s' => \$options{kmerhash},
+    'p|paired'     => \$options{paired}
     ) || die ("Error in command line arguments\n");
 
 # prepare input files
 $options{inputfiles} = [ split(',', join(",", @{$options{inputfiles}})) ];
+
+# check if paired is set... I this case, we require an even number of input files
+if ($options{paired} && @{$options{inputfiles}}%2!=0)
+{
+    die "Number of inputfiles is not even!\n";
+}
 
 # same for the kmerlib files
 $kmer_cache = Storable::retrieve($options{kmerhash});
@@ -35,9 +42,25 @@ if (exists $options{outputfile} && defined $options{outputfile})
 }
 
 # go through the input files
-foreach my $inputfile (@{$options{inputfiles}})
+for (my $i = 0; $i < @{$options{inputfiles}}; $i++)
 {
+    my $inputfile = $options{inputfiles}->[$i];
+
+    my $inputfile2;
+
     my $filesize = -s $inputfile;
+
+    # if paired mode, we need the second input file
+    if ($options{paired})
+    {
+	# increase the counter $i++;
+	$i++;
+
+	# now at position [$i] is the second file
+	$inputfile2 = $options{inputfiles}->[$i];
+	$filesize += -s $inputfile2;
+    }
+
     my $progress = Term::ProgressBar->new(
 	{
 	    name  => 'Kmer File',
@@ -52,11 +75,23 @@ foreach my $inputfile (@{$options{inputfiles}})
 
     open(INPUT, "<", $inputfile) || die "Unable to open input file '$inputfile': $!";
 
-    while(! eof(INPUT))
+    my $in2 = undef;
+    if ($options{paired})
+    {
+	open($in2, "<", $inputfile2) || die "Unable to open input file '$inputfile2': $!";
+    }
+
+    while(!( eof(INPUT) || ( defined $in2 && eof($in2))))
     {
 
-	if ( tell(INPUT) > $next_update ) {
-	    $progress->update( tell(INPUT) );
+	my $pos = tell(INPUT);
+	if ($options{paired})
+	{
+	    $pos += tell($in2);
+	}
+
+	if ( $pos > $next_update ) {
+	    $progress->update( $pos );
 	    $next_update += $update_block_size;
 	}
 	my ($header, $seq, $header2, $qual) = (scalar <INPUT>, scalar <INPUT>, scalar <INPUT>, scalar <INPUT>);
@@ -65,6 +100,18 @@ foreach my $inputfile (@{$options{inputfiles}})
 	chomp($seq);
 	chomp($header2);
 	chomp($qual);
+
+	my ($second_header, $second_seq, $second_header2, $second_qual);
+
+	if ($options{paired})
+	{
+	    ($second_header, $second_seq, $second_header2, $second_qual) = (scalar <$in2>, scalar <$in2>, scalar <$in2>, scalar <$in2>);
+
+	    chomp($second_header);
+	    chomp($second_seq);
+	    chomp($second_header2);
+	    chomp($second_qual);
+	}
 
 	# create the kmers and check for each kmer if the kmer is present
 	my ($num_kmers, $num_valid_kmers, $num_unknown_kmers) = (0, 0, 0);
@@ -89,17 +136,51 @@ foreach my $inputfile (@{$options{inputfiles}})
 	    }
 	}
 
+	if ($options{paired})
+	{
+	    foreach my $k (kmerize($second_seq, 19))
+	    {
+		$num_kmers++;
+
+		my ($valid_kmer, $kmer_count) = get_validity_and_kmer_count($k);
+		if ($valid_kmer)
+		{
+		    $num_valid_kmers++;
+		}
+
+                # if kmer_count was -1 the kmer was unknown, therefore increase the counter and ignore that coverage for coverage calculation
+		if ($kmer_count == -1)
+		{
+		    $num_unknown_kmers++;
+		} else {
+		    push(@kmer_counts, $kmer_count);
+		}
+	    }
+	}
+
 	my $percentage_valid_kmers = $num_valid_kmers/$num_kmers;
 	my ($mean_coverage, $median_coverage) = calc_mean_median(\@kmer_counts);
 	printf $outfh "%s percent_valid:%.5f mean_coverage:%.1f median_coverage:%.1f num_unknown_kmers_ignored_for_coverage: %d\n%s\n%s\n%s\n",
 	$header, $percentage_valid_kmers, $mean_coverage, $median_coverage, $num_unknown_kmers, $seq, $header2, $qual;
-    }
 
-    if ( $filesize >= $next_update ) {
-	$progress->update($filesize);
+	if ($options{paired})
+	{
+	    printf $outfh "%s percent_valid:%.5f mean_coverage:%.1f median_coverage:%.1f\n%s\n%s\n%s\n",
+	    $second_header, $percentage_valid_kmers, $mean_coverage, $median_coverage, $second_seq, $second_header2, $second_qual;
+	}
+
+	if ( $filesize >= $next_update ) {
+	    $progress->update($filesize);
+	}
+
     }
 
     close(INPUT) || die "Unable to close input file '$inputfile': $!";
+    if ($options{paired})
+    {
+	close($in2) || die "Unable to close input file '$inputfile2': $!";
+    }
+
 }
 
 # close the output file
